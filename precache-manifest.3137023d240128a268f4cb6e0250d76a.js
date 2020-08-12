5bb50a8894fc8cf4ec9c832627d7e48f7434542c
Marcio Git <marcioseo@gmail.com>
2020-08-12 17:48:23 +1000

    Updates

---
+++ precache-manifest.3137023d240128a268f4cb6e0250d76a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a479fadc99a0d4f9de33721d843b86",
+    "url": "/resolve-dialogs/index.html"
+  },
+  {
+    "revision": "8d14bff4d0a9551b9d0e",
+    "url": "/resolve-dialogs/static/js/2.5fdf9597.chunk.js"
+  },
+  {
+    "revision": "0472107b6f246c1e2ca64f14170aecd1",
+    "url": "/resolve-dialogs/static/js/2.5fdf9597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e432941d288617caefb",
+    "url": "/resolve-dialogs/static/js/main.bc0d8732.chunk.js"
+  },
+  {
+    "revision": "1b798894f5303a7a9d5f",
+    "url": "/resolve-dialogs/static/js/runtime-main.273747ec.js"
+  }
+]);
